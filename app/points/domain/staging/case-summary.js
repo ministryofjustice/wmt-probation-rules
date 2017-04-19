3f6f68816540db0d40325cfab0aaa08699fa4091
Tom Swann <ts.swann@gmail.com>
2017-04-19 11:43:41 +0100

    Workload points staging domain model - Objects to act as contract for ETL process - OM Workload top level object representing an individuals workload - Associated objects to represent:   - Court Reports   - Inst Reports   - Case Summary   - Case Details

---
+++ app/points/domain/staging/case-summary.js
@@ -0,0 +1,41 @@
+class CaseSummary {
+  constructor (
+    trust,
+    regionDesc,
+    regionCode,
+    lduDesc,
+    lduCode,
+    teamDesc,
+    teamCode,
+    omSurname,
+    omForename,
+    omGradeCode,
+    omKey,
+    communityTiers,
+    licenseTiers,
+    custodyTiers,
+    comIn1st16Weeks,
+    licIn1st16Weeks,
+    datestamp
+  ) {
+    this.trust = trust
+    this.regionDesc = regionDesc
+    this.regionCode = regionCode
+    this.lduDesc = lduDesc
+    this.lduCode = lduCode
+    this.teamDesc = teamDesc
+    this.teamCode = teamCode
+    this.omSurname = omSurname
+    this.omForename = omForename
+    this.omGradeCode = omGradeCode
+    this.omKey = omKey
+    this.communityTiers = communityTiers
+    this.licenseTiers = licenseTiers
+    this.custodyTiers = custodyTiers
+    this.comIn1st16Weeks = comIn1st16Weeks
+    this.licIn1st16Weeks = licIn1st16Weeks
+    this.datestamp = datestamp
+  }
+}
+
+module.exports = CaseSummary
---
+++ app/points/domain/staging/om-workload.js
@@ -0,0 +1,10 @@
+class OmWorkload {
+  constructor (caseSummary, courtReports, instReports, caseDetails) {
+    this.caseSummary = caseSummary
+    this.courtReports = courtReports
+    this.instReports = instReports
+    this.caseDetails = caseDetails
+  }
+}
+
+module.exports = OmWorkload
