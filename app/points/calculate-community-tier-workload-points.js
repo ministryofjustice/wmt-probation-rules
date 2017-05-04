38d4c3263178227cc99cda4298b7ea3cf8fd0add
Damien Coney <d.coney@kainos.com>
2017-05-04 09:41:09 +0100

    332: Changing file names as requested. Question over unit tests

--- app/points/calculate-community-tier-workload-points.js
+++ app/points/calculate-community-tier-workload-points.js
@@ -5,14 +5,11 @@ module.exports = function (communityTiers, caseTypeWeightings, commTierCPEnabled
   communityTiers.getTierCountsAsList().forEach(function (tier) {
     communityWorkloadPoints += calculatePointsForTier(tier.tierCount, tier.points, caseTypeWeightings)
   }, this)
-  console.log(communityWorkloadPoints)
   if (commTierCPEnabled) {
     communityWorkloadPoints += calculatePointsForTier(communityTiers.a1.tierCount, communityTiers.a1.points, caseTypeWeightings.commTierCPWeighting)
   }
-  console.log(communityWorkloadPoints)
   if (commTier3AEnabled) {
     communityWorkloadPoints += calculatePointsForTier(communityTiers.a3.tierCount, communityTiers.a3.points, caseTypeWeightings)
   }
-  console.log(communityWorkloadPoints)
   return communityWorkloadPoints
 }
--- test/unit/points/test-calculate-license-tier-workload-points.js
+++ test/unit/points/test-calculate-license-tier-workload-points.js
@@ -1,9 +1,9 @@
 const expect = require('chai').expect
-const calculateLienseWorkloadPoints = require('../../../app/points/calculate-license-workload-points')
+const calculateLienseWorkloadPoints = require('../../../app/points/calculate-license-tier-workload-points')
 const pointsHelper = require('../../helpers/points-helper')
 const Locations = require('../../../app/staging/constants/locations')
 
-describe('points/calculate-license-workload-points', function () {
+describe('points/calculate-license-tier-workload-points', function () {
   it('should calculate the value without error', function () {
     var tiersObject = pointsHelper.getTestTiersObject(Locations.LICENSE)
     var weightings = pointsHelper.getWeightings()
