fba184b578ff24792718d021096b7921c83c0b55
Damien Coney <d.coney@kainos.com>
2017-05-04 16:04:27 +0100

    333: Added logic as comments to the respective files for customising the logic. Unsure how the data will be structured going into the methods but added dummy methods and classes for now.

---
+++ app/points/calculate-base-license-workload-values.js
@@ -0,0 +1,15 @@
+module.export = function () {
+  
+}
+
+// COUNT (case details)
+// WHERE
+// Location == ‘license’ AND
+// TierCode == empty string OR TierCode == zero
+// AND RowType == (O, W or U depending on postfix)
+
+// COUNT (case details)
+// WHERE
+// Location == ‘license’ AND
+// TierCode == [1 - 7] AND
+// RowType == (O, W or U depending on postfix)
