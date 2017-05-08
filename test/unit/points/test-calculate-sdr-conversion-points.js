6f1934ca632bd84a5314c6ba2b5da0f16e2db23a
Steven Cochrane <s.cochrane@kainos.com>
2017-05-08 09:24:41 +0100

    fixed issues flagged by standard config update

--- test/unit/points/test-calculate-sdr-conversion-points.js
+++ test/unit/points/test-calculate-sdr-conversion-points.js
@@ -2,7 +2,6 @@ const expect = require('chai').expect
 const calculateSdrConversionPoints = require('../../../app/points/calculate-sdr-conversion-points')
 
 describe('points/calculate-sdr-conversion-points', function () {
-
   var SDR_CONVERSIONS_LAST_30_DAYS = 20
   var WORKLOAD_POINTS_SDR_CONVERSION = 30
 
@@ -10,5 +9,4 @@ describe('points/calculate-sdr-conversion-points', function () {
     expect(calculateSdrConversionPoints(SDR_CONVERSIONS_LAST_30_DAYS, WORKLOAD_POINTS_SDR_CONVERSION))
       .to.equal(600)
   })
-
 })
