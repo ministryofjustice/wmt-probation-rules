81489ad9d52a494aa1585cf913ab7775b23c5ae5
Damien Coney <d.coney@kainos.com>
2017-05-10 16:11:44 +0100

    333: Remove unnecessary files and add base file

---
+++ test/unit/points/test-calculate-base-workload-values.js
@@ -0,0 +1,16 @@
+const expect = require('chai').expect
+const calculateBaseWorkloadValues = require('../../../app/points/calculate-base-workload-values')
+// const pointsHelper = require('../../../helpers/points-helper')
+const stagingHelper = require('../../helpers/staging-helper')
+const locations = require('../../../app/staging/constants/locations')
+
+describe('points/calculate-base-workload', function () {
+  it('should calculate the base workload values for each offender manager workload', function () {
+    var caseRefNo = stagingHelper.getGeneratedCaseRefNo()
+    var omKey = '1234'
+    var omWorkload = stagingHelper.getTestCaseDetails(caseRefNo, omKey, locations.COMMUNITY)
+
+    var resolvedCases = calculateBaseWorkloadValues(omWorkload)
+    expect(resolvedCases).to.not.be.NaN()
+  })
+})
