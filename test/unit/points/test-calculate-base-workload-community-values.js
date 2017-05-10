95348f2778166e1d40c082f7bd30fb769e1e112b
Damien Coney <d.coney@kainos.com>
2017-05-10 16:11:44 +0100

    333: Remove unnecessary files and add base file

--- test/unit/points/test-calculate-base-workload-community-values.js
+++
@@ -1,5 +0,0 @@
-const expect = require('chai').expect
-
-describe('points/calculate-base-workload-comomunity-values', function () {
-
-})
--- test/unit/points/test-calculate-base-workload-custody-values.js
+++
@@ -1,5 +0,0 @@
-const expect = require('chai').expect
-
-describe('points/calculate-base-workload-custody-values', function () {
-
-})
