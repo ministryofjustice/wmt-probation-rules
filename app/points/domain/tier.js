50b4b660dcefb61494b39ecee9abab8050c90b9c
Damien Coney <d.coney@kainos.com>
2017-05-18 10:29:23 +0100

    333: New branch with cherrypicked commits and a clean history

--- app/points/domain/tier.js
+++ app/points/domain/tier.js
@@ -4,6 +4,7 @@ const TierCounts = require('../../../app/points/domain/tier-counts')
 class Tier {
   constructor (tierCounts, points) {
     this.tierCounts = tierCounts
+    this.points = points
     this.isValid()
   }
 
