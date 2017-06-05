ec3e79dd1e3110e78cd6f8ee5363309b922c121c
Steven Cochrane <s.cochrane@kainos.com>
2017-06-05 15:44:46 +0100

    WMT-417: updated probation-rules to expose domain models

--- probation-rules.js
+++ probation-rules.js
@@ -1,12 +1,20 @@
 const OmWorkload = require('./app/staging/domain/om-workload')
 const CasesSummary = require('./app/staging/domain/cases-summary')
 const CaseDetails = require('./app/staging/domain/case-details')
-
 const stagingHelper = require('./test/helpers/staging-helper')
+const OffenderManager = require('./app/points/domain/offender-manager')
+const Team = require('./app/points/domain/team')
+const WorkloadOwner = require('./app/points/domain/workload-owner')
+const WorkingHours = require('./app/points/domain/working-hours')
+
 
 module.exports = {
   OmWorkload: OmWorkload,
   CasesSummary: CasesSummary,
   CaseDetails: CaseDetails,
-  stagingTestHelper: stagingHelper
+  stagingTestHelper: stagingHelper,
+  OffenderManager: OffenderManager,
+  Team: Team,
+  WorkloadOwner: WorkloadOwner,
+  WorkingHours: WorkingHours
 }
