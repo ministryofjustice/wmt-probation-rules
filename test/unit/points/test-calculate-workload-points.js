c07090ab62835c1d0dfaa47d22991874cc02ad27
Caoimhe Boyle <c.boyle@kainos.com>
2017-07-25 14:52:30 +0100

    Rename calculate-total-workload-points to calculate-workload-points

--- test/unit/points/test-calculate-workload-points.js
+++ test/unit/points/test-calculate-workload-points.js
@@ -1,22 +1,22 @@
 const expect = require('chai').expect
-const calculateTotalWorkloadPoints = require('../../../app/points/calculate-total-workload-points')
+const calculateWorkloadPoints = require('../../../app/points/calculate-workload-points')
 const pointsHelper = require('../../helpers/points-helper')
 
-describe('points/calculate-total-workload-points', function () {
+describe('points/calculate-workload-points', function () {
   it('succesfully calculates the total points with weightings and flags set', function () {
     var workloadObject = pointsHelper.getTestWorkloadObject()
     var weightings = pointsHelper.getWeightings()
 
-    var result = calculateTotalWorkloadPoints(workloadObject, weightings)
+    var result = calculateWorkloadPoints(workloadObject, weightings)
 
     expect(result).to.equal(240)
   })
   it('should throw an error when the workload object is undefined', function () {
     var weightings = pointsHelper.getWeightings()
-    expect(function () { calculateTotalWorkloadPoints(undefined, weightings) }).to.throw(Error)
+    expect(function () { calculateWorkloadPoints(undefined, weightings) }).to.throw(Error)
   })
   it('should throw an error when the workload object is undefined', function () {
     var workloadObject = pointsHelper.getTestWorkloadObject()
-    expect(function () { calculateTotalWorkloadPoints(workloadObject, undefined) }).to.throw(Error)
+    expect(function () { calculateWorkloadPoints(workloadObject, undefined) }).to.throw(Error)
   })
 })
