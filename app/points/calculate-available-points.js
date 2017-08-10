bcf9a364603770a83541d17fd290dac9ba279562
Damien Coney <d.coney@kainos.com>
2017-08-10 10:50:55 +0100

    787: Move contracted hours into own calculation method to allow it to be used in the frontend

--- app/points/calculate-available-points.js
+++ app/points/calculate-available-points.js
@@ -1,5 +1,4 @@
-const DefaultContractedHours = require('./domain/default-contracted-hours')
-const OM_TYPE_IDS = require('./constants/offender-manager-type-ids')
+const getHours = require('./calculate-contracted-hours')
 
 var calculateAvailablePoints = function (nominalTarget, baseHours, hoursReduction, defaultContractedHoursForBand) {
   return (nominalTarget * (baseHours / defaultContractedHoursForBand)) * ((baseHours - hoursReduction) / baseHours)
@@ -7,30 +6,9 @@ var calculateAvailablePoints = function (nominalTarget, baseHours, hoursReductio
 
 module.exports = function (nominalTarget, offenderManagerTypeId, contractedHoursPerWeek,
   hoursReduction, defaultContractedHours) {
-  if (!(defaultContractedHours instanceof DefaultContractedHours)) {
-    throw new Error('defaultContractedHours should be an instance of DefaultContractedHours')
-  }
-
-  const UNSUPPORTED_BANDS = [OM_TYPE_IDS.UNSUPPORTED_A, OM_TYPE_IDS.UNSUPPORTED_B]
-  const PSO_BANDS = [OM_TYPE_IDS.PSO, OM_TYPE_IDS.PSO_B, OM_TYPE_IDS.PSO_C]
-
   var availablePoints = 0
-
-  if (!(UNSUPPORTED_BANDS.indexOf(offenderManagerTypeId) >= 0)) {
-    var defaultContractedHoursForBand = 0
-
-    if (PSO_BANDS.indexOf(offenderManagerTypeId) >= 0) {
-      defaultContractedHoursForBand = defaultContractedHours.pso
-    } else {
-      defaultContractedHoursForBand = defaultContractedHours.po
-    }
-
-    var baseHours = typeof contractedHoursPerWeek !== 'number'
-      ? defaultContractedHoursForBand : contractedHoursPerWeek
-
-    availablePoints =
-      calculateAvailablePoints(nominalTarget, baseHours, hoursReduction, defaultContractedHoursForBand)
-  }
+  var resultHours = getHours(contractedHoursPerWeek)
+  availablePoints = calculateAvailablePoints(nominalTarget, resultHours.baseHours, hoursReduction, resultHours.defaultContractedHoursForBand)
 
   return parseInt(availablePoints, 10)
 }
