0f9d10040aa1bdffdbcc32207c8c58e60be43ccb
Damien Coney <d.coney@kainos.com>
2017-08-16 15:00:34 +0100

    787: Adding more test cases for calculate contracted hours

--- test/unit/points/test-calculate-contracted-hours.js
+++ test/unit/points/test-calculate-contracted-hours.js
@@ -2,6 +2,8 @@ const calculateContractedHours = require('../../../app/points/calculate-contract
 const DefaultContractedHours = require('../../../app/points/domain/default-contracted-hours')
 const expect = require('chai').expect
 
+const OM_TYPE_IDS = require('../../../app/points/constants/offender-manager-type-ids')
+
 describe('points/calculate-contracted-hours', function () {
   var contractedHoursPerWeek
   var defaultContractedHours
@@ -21,5 +23,34 @@ describe('points/calculate-contracted-hours', function () {
   it('should return contracted hours', function () {
     offenderManagerTypeId = 1
     expect(calculateContractedHours(contractedHoursPerWeek, defaultContractedHours, offenderManagerTypeId)).to.be.an('object')
+  })
+
+  it(`returns default PSO contracted hours when offender manager type ID is ${OM_TYPE_IDS.PSO}`, function () {
+    expect(calculateContractedHours(contractedHoursPerWeek, defaultContractedHours, OM_TYPE_IDS.PSO)).to.be.an('object')
+  })
+
+  it(`returns default PSO contracted hours when offender manager type ID is ${OM_TYPE_IDS.PSO_B}`, function () {
+    expect(calculateContractedHours(contractedHoursPerWeek, defaultContractedHours, OM_TYPE_IDS.PSO_B)).to.be.an('object')
+  })
+
+  it(`returns default PSO contracted hours when offender manager type ID is ${OM_TYPE_IDS.PSO_C}`, function () {
+    expect(calculateContractedHours(contractedHoursPerWeek, defaultContractedHours, OM_TYPE_IDS.PSO_C)).to.be.an('object')
+  })
+
+  it('returns other default contracted hours when offender manager type ID is an unexpected number', function () {
+    expect(calculateContractedHours(contractedHoursPerWeek, defaultContractedHours, 1)).to.be.an('object')
+  })
+
+  it('uses defaultContractedHoursForBand when contractedHoursPerWeek is not provided', function () {
+    // TODO: check if zero for this is an acceptable answer or should it use defaultContractedHoursForBand too.
+    expect(calculateContractedHours(null, defaultContractedHours, OM_TYPE_IDS.PSO_B)).to.be.an('object')
+  })
+
+  it(`returns null when offender manager type ID is ${OM_TYPE_IDS.UNSUPPORTED_A}`, function () {
+    expect(calculateContractedHours(contractedHoursPerWeek, defaultContractedHours, OM_TYPE_IDS.UNSUPPORTED_A)).to.be.null
+  })
+
+  it(`returns null when offender manager type ID is ${OM_TYPE_IDS.UNSUPPORTED_B}`, function () {
+    expect(calculateContractedHours(contractedHoursPerWeek, defaultContractedHours, OM_TYPE_IDS.UNSUPPORTED_B)).to.be.null
   })
 })
