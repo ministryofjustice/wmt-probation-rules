dece35cd8e7794888ff52531eaa02fd241898e84
Andrew Wright <andrew.js.wright@gmail.com>
2017-10-04 11:34:42 +0100

    Add ARMS assessmens to case type weightings
    * 414: Add ARMS assessments to case type weightings
    
    * Update case-type-weightings to accept required ARMS weightings

--- app/points/domain/case-type-weightings.js
+++ app/points/domain/case-type-weightings.js
@@ -1,10 +1,12 @@
 const assertNumber = require('./validation/assert-number')
 
 class CaseTypeWeightings {
-  constructor (warrants, unpaidWork, overdueTermination, pointsConfiguration) {
+  constructor (warrants, unpaidWork, overdueTermination, armsCommunity, armsLicense, pointsConfiguration) {
     this.warrants = warrants
     this.unpaidWork = unpaidWork
     this.overdueTermination = overdueTermination
+    this.armsCommunity = armsCommunity
+    this.armsLicense = armsLicense
     this.pointsConfiguration = pointsConfiguration
     this.isValid()
   }
@@ -13,6 +15,8 @@ class CaseTypeWeightings {
     assertNumber(this.warrants, 'warrants')
     assertNumber(this.unpaidWork, 'unpaidWork')
     assertNumber(this.overdueTermination, 'overdueTermination')
+    assertNumber(this.armsCommunity, 'armsCommunity')
+    assertNumber(this.armsLicense, 'armsLicense')
   }
 }
 
--- test/unit/points/domain/test-case-type-weightings.js
+++ test/unit/points/domain/test-case-type-weightings.js
@@ -4,23 +4,29 @@ const CaseTypeWeightings = require('../../../../app/points/domain/case-type-weig
 
 describe('points/domain/case-type-weightings', function () {
   it('throws an error when any property is undefined', function () {
-    expect(function () { new CaseTypeWeightings(undefined, 1, 1, []) }).to.throw(Error)
-    expect(function () { new CaseTypeWeightings(1, undefined, 1, []) }).to.throw(Error)
-    expect(function () { new CaseTypeWeightings(1, 1, undefined, []) }).to.throw(Error)
+    expect(function () { new CaseTypeWeightings(undefined, 1, 1, 1, 1, []) }).to.throw(Error)
+    expect(function () { new CaseTypeWeightings(1, undefined, 1, 1, 1, []) }).to.throw(Error)
+    expect(function () { new CaseTypeWeightings(1, 1, undefined, 1, 1, []) }).to.throw(Error)
+    expect(function () { new CaseTypeWeightings(1, 1, 1, undefined, 1, []) }).to.throw(Error)
+    expect(function () { new CaseTypeWeightings(1, 1, 1, 1, undefined, []) }).to.throw(Error)
   })
   it('throws an error when any property is a string', function () {
-    expect(function () { new CaseTypeWeightings('input', 1, 1, []) }).to.throw(Error)
-    expect(function () { new CaseTypeWeightings(1, 'input', 1, []) }).to.throw(Error)
-    expect(function () { new CaseTypeWeightings(1, 1, 'input', []) }).to.throw(Error)
+    expect(function () { new CaseTypeWeightings('input', 1, 1, 1, 1, []) }).to.throw(Error)
+    expect(function () { new CaseTypeWeightings(1, 'input', 1, 1, 1, []) }).to.throw(Error)
+    expect(function () { new CaseTypeWeightings(1, 1, 'input', 1, 1, []) }).to.throw(Error)
+    expect(function () { new CaseTypeWeightings(1, 1, 1, 'input', 1, []) }).to.throw(Error)
+    expect(function () { new CaseTypeWeightings(1, 1, 1, 1, 'input', []) }).to.throw(Error)
   })
   it('doesn\'t throw an error when all the parameters are numbers', function () {
-    expect(function () { new CaseTypeWeightings(1, 1, 1, []) }).to.not.throw(Error)
+    expect(function () { new CaseTypeWeightings(1, 1, 1, 1, 1, []) }).to.not.throw(Error)
   })
   it('retrieves all values', function () {
-    var caseTypeWeightings = new CaseTypeWeightings(1, 2, 3, [10, 10, 10, 10, 10, 10, 10, 10])
+    var caseTypeWeightings = new CaseTypeWeightings(1, 2, 3, 4, 5, [10, 10, 10, 10, 10, 10, 10, 10])
     expect(caseTypeWeightings.warrants).to.equal(1)
     expect(caseTypeWeightings.unpaidWork).to.equal(2)
     expect(caseTypeWeightings.overdueTermination).to.equal(3)
+    expect(caseTypeWeightings.armsCommunity).to.equal(4)
+    expect(caseTypeWeightings.armsLicense).to.equal(5)
     expect(caseTypeWeightings.pointsConfiguration[0]).to.equal(10)
   })
 })
