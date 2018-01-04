526ee0cdd5ab3abe9e0223ac446cb234a2bcba16
David McKinney <d.mckinney@kainos.com>
2018-01-04 16:24:13 +0000

    Expect and probation rules were assigned values but never used

--- test/unit/probation-rules.js
+++ test/unit/probation-rules.js
@@ -1,5 +1,3 @@
-const expect = require('chai').expect
-const probationRules = require('../../probation-rules')
 
 describe('probation-rules API', function () {
   it('can be required without crashing', function () {
