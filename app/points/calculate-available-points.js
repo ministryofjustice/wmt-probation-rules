6f3601f9aafa1e80ebf30cc962a6fa416ff1386c
David McKinney <d.mckinney@kainos.com>
2018-01-04 16:16:18 +0000

    Prevent available points calculation from performing divide by zero error when contracted hours are zero

--- app/points/calculate-available-points.js
+++ app/points/calculate-available-points.js
@@ -8,7 +8,7 @@ module.exports = function (nominalTarget, offenderManagerTypeId, contractedHours
   hoursReduction, defaultContractedHours) {
   var availablePoints = 0
   var resultHours = getHours(contractedHoursPerWeek, defaultContractedHours, offenderManagerTypeId)
-  if (resultHours !== null) {
+  if (resultHours !== null && resultHours.baseHours !== 0) {
     availablePoints = calculateAvailablePoints(nominalTarget, resultHours.baseHours, hoursReduction, resultHours.defaultContractedHoursForBand)
   }
   return parseInt(availablePoints, 10)
